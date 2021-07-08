8ec07bf27bdfd676e0c32be443d1b127aa31fb33
MarcoMartiniano <79107727+MarcoMartiniano@users.noreply.github.com>
2021-07-07 22:35:04 -0300

    Primeiro Commit
    Primeiro Commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function btnOnClickOuviragora(){
+    window.open("https://www.youtube.com/watch?v=QCqcg5uiNWk");
+}
+
+
+function btnOnClickInfo(){
+    window.open("https://pt.wikipedia.org/wiki/Moonglow_(%C3%A1lbum)");
+}
+
